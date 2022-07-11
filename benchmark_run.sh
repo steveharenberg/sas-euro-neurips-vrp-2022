a193bad9ab5bdaab657d786739ca00d3a6be56e0
Brandon Reese <chief.brandon@gmail.com>
2022-07-10 23:10:44 -0400

    Add 'random' premade strategy

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -25,7 +25,7 @@ staticFlag="--static" # set to "--static" for static or "" for dynamic
 epochTime=5 # time limit for one epoch
 
 # Solver-specific Parameters
-strategy=greedy
+strategy=random
 verboseFlag=""
 
 # Read the list of instances

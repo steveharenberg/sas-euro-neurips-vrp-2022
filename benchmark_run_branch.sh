572180ff328bd34aaf75681131b39c3307a00c3e
Brandon Reese <Brandon.Reese@sas.com>
2022-07-23 15:14:15 -0400

    Static, 60 seconds

--- benchmark_run_branch.sh
+++ benchmark_run_branch.sh
@@ -2,5 +2,5 @@
 
 tempBranch=`git rev-parse --abbrev-ref HEAD`
 git checkout --quiet $1
-bash ./benchmark_run.sh -i instances_25.txt -t $1 -n 25
+bash ./benchmark_run.sh -i instances_25.txt -t $1 -e 60 --static -n 25
 git checkout --quiet $tempBranch

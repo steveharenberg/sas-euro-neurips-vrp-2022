a4a71d35080951183b1c4c531400870b069d030b
Brandon Reese <Brandon.Reese@sas.com>
2022-07-25 11:10:07 -0400

    Add dynamic version of script

---
+++ benchmark_run_branch_dynamic.sh
@@ -0,0 +1,6 @@
+# run benchmark_run.sh for the strategy saved in the branch given by first parameter
+
+tempBranch=`git rev-parse --abbrev-ref HEAD`
+git checkout --quiet $1
+bash ./benchmark_run.sh -i instances_25.txt -t D$1 -e 60 -n 25
+git checkout --quiet $tempBranch

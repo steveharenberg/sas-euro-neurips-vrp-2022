d28334df862684ce096ae5696ea9c2db09c98794
sthare <steven.harenberg@sas.com>
2022-08-03 09:42:07 -0400

    include commented vroom option

--- benchmark_run_random_vary.sh
+++ benchmark_run_random_vary.sh
@@ -2,11 +2,14 @@
 
 tempBranch=`git rev-parse --abbrev-ref HEAD`
 BRANCH=strategy_random
+# BRANCH=strategy_vroom_random
 
+git stash
 git checkout --quiet $BRANCH
-for p in {0..100..5}
+for p in {0..100..10}
 do
     c=$(echo "$p*0.01"|bc)
     bash ./benchmark_run.sh -i instances_25.txt -t D${BRANCH}_$c -e 60 -n 5 --randomprob $c
 done
 git checkout --quiet $tempBranch
+git stash pop

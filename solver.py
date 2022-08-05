e82d52a355aad98e9cb2683bf1839c344179799e
sthare <steven.harenberg@sas.com>
2022-08-04 22:58:57 -0400

    Add preprocessTimeWindows argument to solver

--- solver.py
+++ solver.py
@@ -40,6 +40,7 @@ ALL_HGS_ARGS = [
     "skipSwapStarDist",
     "circleSectorOverlapToleranceDegrees",
     "minCircleSectorSizeDegrees",
+    "preprocessTimeWindows",
 ]
 
 def solve_static_vrptw(instance, time_limit=3600, tmp_dir="tmp", seed=1, args=None):
@@ -236,6 +237,8 @@ if __name__ == "__main__":
     parser.add_argument("--skipSwapStarDist", type=int)
     parser.add_argument("--circleSectorOverlapToleranceDegrees", type=int)
     parser.add_argument("--minCircleSectorSizeDegrees", type=int)
+
+    parser.add_argument("--preprocessTimeWindows", type=int, default=0, choices={0, 1}, help="Remove (make large cost) links that violate time windows")
 
     args, unknown = parser.parse_known_args()
 

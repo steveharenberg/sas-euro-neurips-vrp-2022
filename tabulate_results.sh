dbbb732c91c4c9e6348761e744408cdc05327ea0
Brandon Reese <Brandon.Reese@sas.com>
2022-08-19 19:54:56 -0400

    Optuna staged pruning script.

--- tabulate_results.sh
+++ tabulate_results.sh
@@ -1,5 +1,12 @@
+if [[ $# -lt 1 ]];then
+   prefix=strategy_
+#    echo "Default prefix: $prefix"
+else
+   prefix=$1 # Get name of current branch. Used as the subdir of the results directory
+#    echo "From Argument 1: prefix: $prefix"
+fi
+
 resultDir=results/
-prefix=strategy_
 
 strategies=($(ls -1 $resultDir |grep ^$prefix))
 numStrategies=${#strategies[@]}
@@ -12,11 +19,11 @@ for i in $(seq 0 $(($numStrategies - 1))); do
         # extract the cost from the second line of results file
         cost=$(head $resultDir${strategies[$i]}/${instances[$j]} -n 2 | tail -n 1 | awk '{print $4}')
 
-        if [[ $# -eq 0 ]]; then
+        if [[ $# -lt 2 ]]; then
             dest=""
             printf "${strategies[$i]}\t${instances[$j]}\t${cost}\n"
         else
-            dest=$1 # table output destination
+            dest=$2 # table output destination
             printf "${strategies[$i]}\t${instances[$j]}\t${cost}\n" > $dest
         fi
     done

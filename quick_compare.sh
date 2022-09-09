17a339ab07574cfee02b0f9c51f6a8fe6a5cbec2
Brandon Reese <Brandon.Reese@sas.com>
2022-09-09 16:39:10 -0400

    wip.

--- quick_compare.sh
+++ quick_compare.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 nw=8
-instanceList=instances_8_small.txt
+# instanceList=instances_8_small.txt
 # instanceList=instances_8_medium.txt
-# instanceList=instances_8_large.txt
+instanceList=instances_8_large.txt
 # instanceList=instances_25.txt
 epochTime=0
 
-PREFIX=quickS
+PREFIX=quickL
 
 for SOLVER_SEED in 1 #2 3 4 5
    do
-   EXTRA_ARGS="-t ${PREFIX}_baseline"
-   ./benchmark_run.sh -i $instanceList -n $nw -e $epochTime -s -d $SOLVER_SEED $EXTRA_ARGS
-   # EXTRA_ARGS="-t ${PREFIX}_manual1 --addReverseOffspring 1"
+   # EXTRA_ARGS="-t ${PREFIX}_baseline1"
    # ./benchmark_run.sh -i $instanceList -n $nw -e $epochTime -s -d $SOLVER_SEED $EXTRA_ARGS
+   EXTRA_ARGS="-t ${PREFIX}_manual1 --fractionGeneratedRandomly 0"
+   ./benchmark_run.sh -i $instanceList -n $nw -e $epochTime -s -d $SOLVER_SEED $EXTRA_ARGS
 done
 echo "Finished. Tabulating results..."
 
-bash "tabulate_results.sh" ${PREFIX} > results/quick-results-S.tsv
+bash "tabulate_results.sh" ${PREFIX} > results/quick-results-L.tsv

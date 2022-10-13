0cb71d6078871cac8587b7e5410afe6a927d41c9
Brandon Reese <Brandon.Reese@sas.com>
2022-10-13 19:56:57 -0400

    Separate seeds for submission

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PASSTHRU_ARGS="${@}"
-python solver.py --solver_seed 212165 --pruneRoutes --strategy fdist --thresholdSchedule 0.85,0.7,0.8,0.75,0.55,0.45,0.3,0.3 --randomGenerator 3 $PASSTHRU_ARGS
+python solver.py --solver_seed 212165 --solver_seed_dynamic 1310 --pruneRoutes --strategy fdist --thresholdSchedule 0.85,0.7,0.8,0.75,0.55,0.45,0.3,0.3 --randomGenerator 3 $PASSTHRU_ARGS

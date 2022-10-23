4b2767deb04597f194dc3a14649fb4f295320059
Brandon Reese <Brandon.Reese@sas.com>
2022-10-23 18:40:49 -0400

    update thresholdSchedule from new optuna run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PASSTHRU_ARGS="${@}"
-python solver.py --solver_seed 212165 --pruneRoutes --strategy fdist --thresholdSchedule 0.85,0.7,0.8,0.75,0.55,0.45,0.3,0.3 --randomGenerator 3 $PASSTHRU_ARGS
+python solver.py --solver_seed 212165 --pruneRoutes --strategy fdist --thresholdSchedule 0.65,1.0,0.8,0.8,0.55,0.15,0.3,0.2 --randomGenerator 3 $PASSTHRU_ARGS
